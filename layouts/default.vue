7dd779211a521087afaa8e1bf8969438c1a0b22a
Enigma <74756147+novb1492@users.noreply.github.com>
2022-11-07 12:31:47 +0900

    네비바 컴포넌트 구성

---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <div>
+        <navBar />
+        <nuxt />
+        
+    </div>
+</template>
+<script>
+import navBar from "./nav/navBar.vue";
+export default {
+    components: { navBar },
+}
+</script>
+<style lang="">
+    
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Tutorial/>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters, mapMutations } from "vuex";
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+  asyncData({isDev, route, store, env, params, query, req, res, redirect, error}) {
+    return {test:'aa'};
+  },
+  mounted(){
+    this.changeNav(1);
+  },
+  methods:{
+    ...mapMutations("navBar", {
+      changeNav: "changeNav",
+    })
+  }
 }
 </script>
