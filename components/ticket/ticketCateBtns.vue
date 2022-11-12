48dcf7fad4505b86ca3e588adeb415c0ec02dcd9
김준영 <novb1492@naver.com>
2022-11-12 15:32:10 +0900

    카테고리 클릭시 미구현 기능 대응 추가

--- components/ticket/ticketCateBtns.vue
+++ components/ticket/ticketCateBtns.vue
@@ -23,14 +23,24 @@
     </div>
 </template>
 <script>
-import { mapMutations } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
+import { ticketPagechangeRouter } from '../../assets/js/jslib';
 
 export default {
     props: ["arr", "flag"],
+    computed: {
+        ...mapGetters("ticket", {
+            locationId:"getLocationId",
+            moveId: "getMoveId",
+            date: "getSelectDate"
+        })
+    },
     methods: {
         ...mapMutations("ticket", {
             changeNum: "changeNum",
-            changeNum2:"changeNum2"
+            changeNum2:"changeNum2",
+            changeLocationId:"changeLocationId",
+            changeMoveId:"changeMoveId"
         }),
         changeCate(cate) {
             if (cate === 1) {
@@ -38,20 +48,32 @@ export default {
                 this.allOn(this.$refs.all_btn, this.$refs.art_house_btn, this.$refs.special_btn);
             } else if (cate === 2) {
                 this.changeNum(2);
+                this.chnageMoveCate();
                 this.artHouseOn(this.$refs.all_btn, this.$refs.art_house_btn, this.$refs.special_btn);
             } else if (cate === 3) {
                 this.changeNum(3);
+                this.chnageMoveCate();
                 this.specialOn(this.$refs.all_btn, this.$refs.art_house_btn, this.$refs.special_btn);
             } else if (cate === 4) {
                 this.changeNum2(1);
                 this.allOn(this.$refs.all_btn2, this.$refs.art_house_btn2, this.$refs.special_btn2);
             } else if (cate === 5) {
                 this.changeNum2(2);
+                this.changeLocaCate();
                 this.artHouseOn(this.$refs.all_btn2, this.$refs.art_house_btn2, this.$refs.special_btn2);
             } else if (cate === 6) {
                 this.changeNum2(3);
+                this.changeLocaCate();
                 this.specialOn(this.$refs.all_btn2, this.$refs.art_house_btn2, this.$refs.special_btn2);
             }
+        },
+        chnageMoveCate(){
+            this.changeMoveId(0);
+            ticketPagechangeRouter(this.$router,0,this.locationId,this.date,{flag:true});
+        },
+        changeLocaCate(){
+            this.changeLocationId(0);
+            ticketPagechangeRouter(this.$router,this.moveId,0,this.date,{flag:true});
         },
         allOn(all_btn, art_house_btn, special_btn) {
             all_btn.classList.add('ticket_m_btn_on');
