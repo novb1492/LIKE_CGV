69c62d198254e153569e0016dc477762b6dee9af
김준영 <novb1492@naver.com>
2022-11-12 17:15:05 +0900

    테스트코드 임시작성

--- store/ticket/ticketAction.js
+++ store/ticket/ticketAction.js
@@ -6,6 +6,15 @@ export default {
      */
     selectMove (context,data) { 
         requestSelect(data);
+        //테스트코드
+        // let response={moveArr:[{ name: '블랙팬서', id: 1, age: 12, advance_rate: 1 ,select:true}, { name: '자백', id: 2, age: 15, advance_rate: 3 ,select:false}, { name: '리멤버', id: 3, age: 12, advance_rate: 2 ,select:false}]
+        //             ,theaterArr : [{ name: '서울', count: 31, id: 1,select:true, local: [{ name: '강남', id: 1 ,select:false,cityId:1}, { name: '강변', id: 2 ,select:false,cityId:1}, { name: '대학로', id: 3 ,select:false,cityId:1}, { name: '구로', id: 4 ,select:false,cityId:1}] }
+        //             , { name: '경기', count: 54, id: 2 ,select:false, local: [{ name: '일산', id: 5 ,select:false,cityId:2}, { name: '고양', id: 6 ,select:false,cityId:2}, { name: '파주', id: 7 ,select:false,cityId:2}, { name: '김포한강', id: 8 ,select:false,cityId:2}] }
+        //             , { name: '인천', count: 11, id: 3 ,select:false, local: [{ name: '계양', id: 9 ,select:false,cityId:3}, { name: '부평', id: 10 ,select:false,cityId:3}, { name: '인천도화', id: 11 ,select:false,cityId:3}, { name: '청라', id: 12 ,select:false,cityId:3}] }
+        //                 ]
+        //             ,dateArr : { year: 2022, month: 11, days: [{ date: 8, can: true, dow: '화', today: true ,select:false}, { date: 9, can: true, dow: '수', today: false ,select:false}, { date: 10, can: true, dow: '목', today: false ,select:false}, { date: 11, can: true, dow: '금', today: false ,select:false}, { date: 12, can: true, dow: '토', today: false ,select:false}, { date: 13, can: true, dow: '일', today: false ,select:false}]}
+        // };
+        // context.commit('changeMoveArr',response.moveArr);
         context.commit('changeMoveId',data.moveId);
         return {flag:true};
     },
